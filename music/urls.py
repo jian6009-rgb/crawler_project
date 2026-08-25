5b064e09cf754993b316f1d3700c1915dd6c1e4b
CHAN_KIN_HOU <cjh25@mails.tsinghua.edu.cn>
2026-08-25 19:35:05 +0800

    feat(music): add pag. artist list

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.song_list, name="song_list"),
-    path("song/<int:song_id>/", views.song_detail, name="song_detail")
+    path("song/<int:song_id>/", views.song_detail, name="song_detail"),
+    path("artist/", views.artist_list, name="artist_list")
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
-from .models import Song
+from .models import Artist, Song
 
 def song_list(request):
     songs = Song.objects.all().order_by("id")
@@ -14,3 +14,11 @@ def song_detail(request, song_id):
     song = get_object_or_404(Song , id=song_id)
 
     return render(request, "music/song_detail.html", {"song": song})
+
+def artist_list(request):
+    artists = Artist.objects.all().order_by("id")
+    paginator = Paginator(artists, 20)
+    page_number = request.GET.get("pageNo")
+    artists = paginator.get_page(page_number)
+
+    return render(request, "music/artist_list.html", {"artists": artists})
