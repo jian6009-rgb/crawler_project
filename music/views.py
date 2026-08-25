c2e58054108ed6de82942a66ff83e94f42be25b9
CHAN_KIN_HOU <cjh25@mails.tsinghua.edu.cn>
2026-08-25 17:51:43 +0800

    feat(music): add song list page

--- music/views.py
+++ music/views.py
@@ -1,6 +1,11 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Song
 
 def song_list(request):
-    songs = Song.objects.all()
+    songs = Song.objects.all().order_by("id")
+    paginator = Paginator(songs, 20)
+    page_number = request.GET.get("pageNo")
+    songs = paginator.get_page(page_number)
+
     return render(request, "music/song_list.html", {"songs": songs})
