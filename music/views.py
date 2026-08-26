a53083599145f18dd1b0c2c2cef43bdd5a105eea
CHAN_KIN_HOU <cjh25@mails.tsinghua.edu.cn>
2026-08-26 12:16:40 +0800

    feat(music): complete artist data and pagination

--- music/views.py
+++ music/views.py
@@ -8,7 +8,13 @@ def song_list(request):
     page_number = request.GET.get("pageNo")
     songs = paginator.get_page(page_number)
 
-    return render(request, "music/song_list.html", {"songs": songs})
+    start = max(1, songs.number - 2)
+    end = min(paginator.num_pages,songs.number + 2)
+    allpages = range(start,end+1)
+    
+
+
+    return render(request, "music/song_list.html", {"songs": songs,"allpages": allpages})
 
 def song_detail(request, song_id):
     song = get_object_or_404(Song , id=song_id)
@@ -20,8 +26,11 @@ def artist_list(request):
     paginator = Paginator(artists, 20)
     page_number = request.GET.get("pageNo")
     artists = paginator.get_page(page_number)
+    start = max(1, artists.number - 2)
+    end = min(paginator.num_pages,artists.number + 2)
+    allpages = range(start,end+1)
 
-    return render(request, "music/artist_list.html", {"artists": artists})
+    return render(request, "music/artist_list.html", {"artists": artists,"allpages": allpages})
 
 def artist_detail(request, artist_id):
     artist = get_object_or_404(Artist , id=artist_id)
